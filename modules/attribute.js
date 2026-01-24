75bcd1242235a7ebee292a26adc145b514cae3ae
Kieren Martin <digitalbilby@gmail.com>
2026-01-25 08:08:14 +1030

    Initial commit as the ability to roll and re-roll attributes is functional

---
+++ modules/attribute.js
@@ -0,0 +1,47 @@
+import { roll_dice_set, clamp } from "./util/utils.js";
+import { attribute_table } from "./data/attribute_data.js";
+
+function roll_attribute (name, short) {
+    let attribute_roll = roll_dice_set(4, 6);
+
+    return get_attribute(name, short, attribute_roll);
+}
+
+function get_empty_attribute(name, short) {
+    return {
+        "base_roll": "--",
+        "name": name,
+        "short": short,
+        "value": "--",
+        "mod": "--",
+        "skill_base": "--",
+        "skill_gain": "-",
+        "damage_dice": [,,"-D-"],
+        "goop": "--"
+    }
+}
+
+function get_attribute(name, short, base_attribute, base_modification = 0) {
+    if (base_attribute === -1) {
+        return get_empty_attribute(name, short)
+    }
+
+    let modded_attribute = base_attribute.total + base_modification
+    let attribute = attribute_table[clamp(modded_attribute - 1, 0, 29)];
+    let goop = attribute_table[clamp(base_attribute.total-1, 0, 29)][5]
+
+    return {
+        "base_roll": base_attribute,
+        "name": name,
+        "short": short,
+        "value": modded_attribute,
+        "mod": attribute[1],
+        "skill_base" : attribute[2],
+        "skill_gain" : attribute[3],
+        "damage_dice": attribute[4],
+        "goop": goop,
+        "roll": base_attribute.total
+    }
+}
+
+export {get_attribute, roll_attribute}
---
+++ modules/util/utils.js
@@ -0,0 +1,37 @@
+function clamp(value, min, max) {
+    if (value < min) {
+        return min
+    }
+    
+    if (value > max) {
+        return max
+    }
+
+    return value
+}
+
+// Roll a single dice between 1 and sides (inclusive)
+function roll_die(sides) {
+    return Math.floor(Math.random() * sides) + 1;
+}
+
+// Roll a set of dice of the same sides.  Returns a total, and the individual results
+function roll_dice_set(number, sides) {
+    let total = 0;
+    let dice_set = [];
+    
+    for (let i = 0; i < number; i++) {
+        let result = roll_die(sides);
+        total += result;
+        dice_set.push(result)
+    }
+
+    return {
+        number: number,
+        sides: sides,
+        total: total,
+        results: dice_set
+    }
+}
+
+export {clamp, roll_die, roll_dice_set}
